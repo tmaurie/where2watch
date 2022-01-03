dfd75f78b300d22adcf1993e3c4904d3b078a42b
tmaurie <thomas.maurier91@gmail.com>
2022-01-03 14:54:05 +0100

    added similar shows

--- src/pages/SerieDetail.vue
+++ src/pages/SerieDetail.vue
@@ -1,26 +1,33 @@
 <template>
   <v-main class="pa-0" v-if="loaded">
     <Detail :get-img-url="getImgUrl(serie.poster_path)" :providers="providers" :item-detail="serie"/>
+    <v-row no-gutters>
+      <ResultList :loaded="loaded" :page="page" :path="path" type="tv" :results="similarShows.slice(0,5)"/>
+    </v-row>
   </v-main>
 </template>
 
 <script>
 import axios from "axios";
 import Detail from "@/components/Detail";
+import ResultList from "@/components/ResultList";
 
 const imageapi = 'https://source.unsplash.com/random'
 const API_KEY = process.env.VUE_APP_API_KEY
 
 export default {
   name: "ItemDetail",
-  components: {Detail},
+  components: {ResultList, Detail},
   data() {
     return {
       colors: [],
       image: imageapi,
       serie: null,
       loaded: false,
-      providers: []
+      similarShows : [],
+      providers: [],
+      page: '1',
+      path: ''
     }
   },
   methods: {
@@ -56,10 +63,25 @@ export default {
             // console.log(response)
           })
           .finally(() => (this.loaded = true))
+    },
+    getSimilarShows() {
+      axios.get(`https://api.themoviedb.org/3/tv/${this.$route.params.id}/recommendations`,
+          {
+            params: {
+              api_key: API_KEY,
+              page: this.page
+            }
+          })
+          .then((response) => {
+            this.similarShows = response.data.results
+            this.loaded = true
+          })
+          .finally(() => (this.loaded = true));
     }
   },
   mounted() {
     this.getItemDetails()
+    this.getSimilarShows()
   },
 }
 </script>
