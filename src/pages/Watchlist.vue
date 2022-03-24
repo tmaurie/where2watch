3cfc9b0dd2c1789e756e71d57626604511cbc541
tmaurie <thomas.maurier91@gmail.com>
2022-03-24 10:18:19 +0100

    added movie/tv toggle to Watchlist

--- src/pages/Watchlist.vue
+++ src/pages/Watchlist.vue
@@ -1,6 +1,22 @@
 <template>
+  <div>
+    <h1 class="text-center">My watchlist</h1>
+    <v-row justify="center" class=" mt-4">
+      <v-btn-toggle v-model="toggle"
+                    @change="getUserWatchlist"
+                    group
+      >
+        <v-btn class="rounded-lg" value="movies">
+          <v-icon>mdi-movie-open</v-icon> &nbsp; Movies
+        </v-btn>
+        <v-btn class="rounded-lg" value="tv">Series &nbsp;
+          <v-icon>mdi-television-classic</v-icon>
+        </v-btn>
+      </v-btn-toggle>
+    </v-row>
+    <ResultList  :loaded="loaded" :page="page" type="movie" :results="results"/>
+  </div>
 
-  <ResultList title="My watchlist"  :loaded="loaded" :page="page" type="movie" :results="results"/>
 </template>
 
 <script>
@@ -11,25 +27,26 @@ export default {
   name: "Watchlist",
   components: {ResultList},
   data() {
-    return{
-      sessionID : localStorage.getItem('session_id'),
-      results : {},
-      info : {},
+    return {
+      sessionID: localStorage.getItem('session_id'),
+      results: {},
+      info: {},
       page: 1,
       path: 'm',
-      loaded : false
+      toggle: 'movies',
+      loaded: false
     }
   },
   mounted() {
     this.getUserWatchlist()
   },
-  methods : {
-    getUserWatchlist(){
-      axios.get(`account/${this.$route.params.id}/watchlist/movies`, {
-        baseURL : process.env.VUE_APP_BASE_URL,
-        params : {
-          api_key : process.env.VUE_APP_API_KEY,
-          session_id : this.sessionID
+  methods: {
+    getUserWatchlist() {
+      axios.get(`account/${this.$route.params.id}/watchlist/${this.toggle}`, {
+        baseURL: process.env.VUE_APP_BASE_URL,
+        params: {
+          api_key: process.env.VUE_APP_API_KEY,
+          session_id: this.sessionID
         }
       }).then((response) => {
         this.results = response.data.results
